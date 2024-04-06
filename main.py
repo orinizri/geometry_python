d66c0293b1f14472e473e6b2f61bcba8cae336ff
oriniz <ori.n@yabi.cloud>
2024-04-06 22:30:42 +0300

    adding some comments

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from classes.point import Point
-from classes.rectangle import Rectangle
-from classes.gui_rectangle import GuiRectangle
-from classes.gui_point import GuiPoint
 from random import randint
 import turtle
+from classes.point import Point
+from classes.gui_point import GuiPoint
+from classes.rectangle import Rectangle
+from classes.gui_rectangle import GuiRectangle
+
+
 # Rectangle Points
 rectangle_bottom_left = Point(1,1)
 rectangle_top_right = Point(6,6)
@@ -21,14 +23,15 @@ rectangle_area = rectangle.area()
 # Check if rectangle and point overlaps
 does_fall_into_rectangle = point.falls_into_rectangle(rectangle)
 
-# print(does_fall_into_rectangle)
-
+# Creating a rectangle
 rectangle_bottom_left = Point(randint(0,100), randint(0,100))
 rectangle_top_right = Point(randint(10,100), randint(10,100))
 gui_rectangle = GuiRectangle(rectangle_bottom_left, rectangle_top_right)
-gui_point = GuiPoint(point.x, point.y)
-    
 
+# Creating a point
+gui_point = GuiPoint(point.x, point.y)
+
+# Drawing the rectangle and the point
 my_turtle = turtle.Turtle()
 gui_rectangle.draw(canvas=my_turtle)
 gui_point.draw(canvas=my_turtle)
